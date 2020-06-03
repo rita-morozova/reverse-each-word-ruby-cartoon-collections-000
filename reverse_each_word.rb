9b65213b3c82aed5f9256bb5334cc994fd273f64
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-03 17:25:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  array.collect do |word|
-    word.reverse
- end
- end
- 
+
 def reverse_each_word(array)
   array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
